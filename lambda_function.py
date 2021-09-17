d9879e1ed6b712ae1a00452129d50cf0d877943f
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-17 14:32:10 -0700

    Test codepipeline

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,8 @@ import os
 from boto3.dynamodb.conditions import Key
 import time
 import uuid
+import json
+import urllib.parse
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
@@ -13,9 +15,6 @@ class DynamoAccessor:
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, id):
-        response = self.table.query(KeyConditionExpression=Key('id').eq(id))
-        return response["Items"][0] if any(response["Items"]) else None
 
     def put_transaction(self, transaction):
         ts = time.time()
@@ -30,7 +29,32 @@ class DynamoAccessor:
         return response
 
 def lambda_handler(event, context):
-    dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.put_transaction(event['transaction'])
-    return db_element
+    s3 = boto3.client('s3')
 
+    # Get the bucket name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    # Get the file from s3
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    try:
+        # Fecth the file from S3
+        response = s3.get_object(Bucket=bucket, key=key)
+
+        text = response["Body"].read().decode()
+        data = json.loads(text)
+
+        # Print the content
+        print(data)
+
+        # Parse and print the transactions
+        # transactions = data['transactions']
+        # for record in transactions:
+        #     print(record['id'])
+        #     print(record['date'])
+        #     print(record['transaction'])
+        # dynamo_backend = DynamoAccessor(DYNAMO_BD)
+        # db_element = dynamo_backend.put_transaction(event['transaction'])
+        return 'Success!'
+    except Exception as e:
+        print(e)
+        raise e
