49e43489b33b4dbb7184c4f12002bee83aa2eed7
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-17 12:59:58 -0700

    Created put_transaction

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,8 @@
 import boto3
 import os
 from boto3.dynamodb.conditions import Key
+from datetime import datetime
+import uuid
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
@@ -15,8 +17,20 @@ class DynamoAccessor:
         response = self.table.query(KeyConditionExpression=Key('id').eq(id))
         return response["Items"][0] if any(response["Items"]) else None
 
+    def put_transaction(self, transaction):
+        now = datetime.now()
+        id = uuid.uuid1()
+        response = self.table.put_item(
+        Item={
+                'id': id,
+                'date': now,
+                'transaction': transaction
+            }
+        )
+        return response
+
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['id'])
+    db_element = dynamo_backend.put_transaction("+5000")
     return db_element
 
