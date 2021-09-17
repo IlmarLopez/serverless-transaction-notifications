0aaf3bbe3649b574a472118c16d41384eda8cc3e
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-17 12:08:33 -0700

    Updated name key

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-west-1 \
---parameter-overrides DynamoName="lablambda" DynamoKey="cc" \
+--parameter-overrides DynamoName="lablambda" DynamoKey="id" \
 --stack-name dynamo-lambda-lab \
 --template-file ./dynamo.yml
