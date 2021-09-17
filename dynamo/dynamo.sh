b97425a7c02cf9c256d6e20f8b509ee56674c1cc
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-16 22:43:31 -0700

    Updated files

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-west-1 \
---parameter-overrides DynamoName="transactions" DynamoKey="uuid" \
+--parameter-overrides DynamoName="transactions" DynamoKey="cc" \
 --stack-name dynamo-lambda-lab \
 --template-file ./dynamo.yml
