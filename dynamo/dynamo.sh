8ea6e4b92229ec68f91b8a4a01234bcfe783241b
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-16 21:22:51 -0700

    Deleted AttributeDefinition items

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-west-1 \
---parameter-overrides DynamoName="lablambda" DynamoKey="uuid" \
+--parameter-overrides DynamoName="transactions" DynamoKey="uuid" \
 --stack-name dynamo-lambda-lab \
 --template-file ./dynamo.yml
