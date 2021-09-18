ec7d937e725f20b9e7f39528abb57ab0d255f1ad
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-17 19:36:34 -0700

    Delete main.py file

--- main.py
+++
@@ -1,9 +0,0 @@
-import statistics
-
-# orders = [60.5, -10.3, -20.46, 10]
-# orders = [-10.3, -20.46]
-orders = [60.5, 10]
-
-average = statistics.mean(orders)
-
-print("The average coffee order price today is $" + str(round(average, 2)))
