92145ec0959e011a843b09ffa1985a6fe2dd2565
Ilmar López <ilmarlopezr@hotmail.com>
2021-09-18 19:57:46 -0700

    Saved transaction records as list

--- lambda_function.py
+++ lambda_function.py
@@ -11,9 +11,9 @@ from botocore.vendored import requests
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
 # # Process an list of records returning the total records per month - format date "M/DD".
-def total_by_month(dates):
+def total_by_month(date_list):
     months_with_total = {}
-    for date in dates:
+    for date in date_list:
         month_name = calendar.month_name[date.month]
         if month_name not in months_with_total:
             months_with_total[month_name] = 1
@@ -26,19 +26,22 @@ class DynamoAccessor:
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def put_transaction(self, transaction):
+    def put_transaction(self, account_statement, txns_list):
         ts = time.time()
         id = uuid.uuid4()
         response = self.table.put_item(
         Item={
                 'id': str(id),
                 'date': str(ts),
-                'transaction': transaction
+                'totalBalance': str(account_statement.get("total_balance")),
+                'averageDebitAmount': str(account_statement.get("average_debit_amount")),
+                'averageCreditAmount': str(account_statement.get("average_credit_amount")),
+                "transactions": txns_list
             }
         )
         return response
 
-def send_html_email(source, destination, data, total_transactions_by_month):
+def send_html_email(source, destination, account_statement, total_transactions_by_month):
     ses_client = boto3.client("ses", region_name="us-west-1")
     CHARSET = "UTF-8"
     # BODY_HTML += "Shepherd {} is on duty.".format(CHARSET)
@@ -61,9 +64,9 @@ def send_html_email(source, destination, data, total_transactions_by_month):
                         <ul>
     """
     
-    BODY_HTML += "<li>Total balance: <strong>$ {}</strong>.</li>".format(data.get("total_balance"))
-    BODY_HTML += "<li>Average debit amount: <strong>$ {}</strong>.</li>".format(data.get("average_debit_amount"))
-    BODY_HTML += "<li>Average credit amount: <strong>$ {}</strong>.</li>".format(data.get("average_credit_amount"))
+    BODY_HTML += "<li>Total balance: <strong>$ {}</strong>.</li>".format(account_statement.get("total_balance"))
+    BODY_HTML += "<li>Average debit amount: <strong>$ {}</strong>.</li>".format(account_statement.get("average_debit_amount"))
+    BODY_HTML += "<li>Average credit amount: <strong>$ {}</strong>.</li>".format(account_statement.get("average_credit_amount"))
 
     BODY_HTML += "</ul>"
     BODY_HTML += "<h5 style=\"margin-bottom: 2px\">Number of transactions by month:</h5>"
@@ -133,22 +136,23 @@ def lambda_handler(event, context):
         file_key_name = event['Records'][0]['s3']['object']['key']
 
         s3 = boto3.client('s3')
-        # dynamo_backend = DynamoAccessor(DYNAMO_BD)
+        dynamo_backend = DynamoAccessor(DYNAMO_BD)
 
         # Fecth the file from S3
         obj = s3.get_object(Bucket=bucket, Key=file_key_name)
         data = obj['Body'].read().decode('utf-8')
-        
+
         transactions = data.split("\n")
 
-        dates = []
+        txns_list = []
+        date_list = []
         total_balance, pos_sum, neg_sum = 0.0, 0.0, 0.0
         pos_count, neg_count = 0, 0
         for r in transactions[1:]:
             r = r.split(",")
+            id_str = str(r[0])
             date_str = r[1]
             date = datetime.strptime(date_str, '%m/%d')
-            dates.append(date)
             amount = float(r[2])
             total_balance += amount
             if amount >= 0:
@@ -158,6 +162,10 @@ def lambda_handler(event, context):
                 neg_count += 1
                 neg_sum += amount
 
+            txns = {"id" : id_str, "date" : date_str, "amount": str(amount)}
+            txns_list.append(txns)
+            date_list.append(date)
+
         average_credit_amount = round(float(pos_sum / pos_count), 2)
         average_debit_amount = round(float(neg_sum / neg_count), 2)
 
@@ -165,19 +173,21 @@ def lambda_handler(event, context):
         print("Average debit amount ", average_debit_amount)
         print("Average credit amount ", average_credit_amount)
 
-        total_transactions_by_month = total_by_month(dates)
+        total_transactions_by_month = total_by_month(date_list)
         for k, v in total_transactions_by_month.items():
             print("Number of transactions in " + k + ":" + str(v))
 
-        # db_element = dynamo_backend.put_transaction()
-        data = {
+        account_statement = {
             "total_balance": total_balance,
             "average_debit_amount": average_debit_amount,
             "average_credit_amount": average_credit_amount
         }
+        db_element = dynamo_backend.put_transaction(account_statement, txns_list)
 
-        print(data)
-        send_html_email("ilmarfranciscol@gmail.com", "ilmarlopezr@hotmail.com", data, total_transactions_by_month)
+        print("txns_list", txns_list)
+
+        print(account_statement)
+        send_html_email("ilmarfranciscol@gmail.com", "ilmarlopezr@hotmail.com", account_statement, total_transactions_by_month)
         return 'Success!'
     except Exception as e:
         print(e)
